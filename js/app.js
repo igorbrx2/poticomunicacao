d7214c4ff024c87f4655a785ffbbdbc61d1bc0e8
igorbrx2 <brx.igor@gmail.com>
2024-02-15 16:31:53 -0300

    criado slider de depoimentos

--- js/app.js
+++ js/app.js
@@ -13,4 +13,32 @@ $(".historiamanifesto a").click(function (e) {
   );
 });
 
+// Slider (depoimentos)
+
+function slider(sliderName, velocidade) {
+  var sliderClass ='.' + sliderName,
+      activeClass = 'active';
+      rotate = setInterval(rotateSlide, velocidade);
+$(sliderClass + ' > :first').addClass(activeClass);
+
+$(sliderClass).hover(function(){
+clearInterval(rotate);
+}, function(){
+  setInterval(rotateSlide, velocidade);
+});
+
+function rotateSlide() {
+  var activeSlide = $(sliderClass +  '> .' + activeClass),
+      nextSlide = activeSlide.next();
+
+  if(nextSlide.length == 0) {
+    nextSlide = $(sliderClass + ' > :first');
+  }
+  activeSlide.removeClass(activeClass);
+  nextSlide.addClass(activeClass);
+}
+}
+
+slider('depoimentos', 4000);
+
 
